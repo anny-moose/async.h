36e7cc4b48641eb770df294b08d90e2ea6ca3e20
annymoose <origami668@gmail.com>
2025-12-27 12:36:04 +0100

    helper library changes

--- queue.h
+++ queue.h
@@ -1,24 +1,19 @@
 #ifndef ANNYMOOSE_QUEUE_H
 #define ANNYMOOSE_QUEUE_H
-#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define ANNYMOOSE_QUEUE_ERROR -1
-#define ANNYMOOSE_QUEUE_SUCCESS 0
-
-typedef struct QueueNode {
+typedef struct queue_node {
     void* data;
-    struct QueueNode* next;
-} QueueNode_t;
+    struct queue_node* next;
+} queue_node_t;
 
 typedef struct {
-    QueueNode_t* head;
-    QueueNode_t* tail;
+    queue_node_t* head;
+    queue_node_t* tail;
     size_t length;
     size_t node_data_size;
     void (*free_data)(void* data);
-} Queue_t;
+} queue_t;
 
 /**
  * Initializes a queue to a state ready for usage.
@@ -26,13 +21,7 @@ typedef struct {
  * @param size_t The size of the data that will be stored in every node.
  * @param free_data Optional function pointer that will be called on every node deletion.
  */
-static inline void Queue_init(Queue_t* queue, size_t data_size, void (*free_data)(void* data)) {
-    queue->head = NULL;
-    queue->tail = NULL;
-    queue->length = 0;
-    queue->node_data_size = data_size;
-    queue->free_data = free_data;
-}
+void queue_init(queue_t* queue, size_t data_size, void (*free_data)(void* data));
 
 /**
  * Allocates a node at the end and copies data to it.
@@ -40,12 +29,49 @@ static inline void Queue_init(Queue_t* queue, size_t data_size, void (*free_data
  * @param data Pointer to the data that will be copied. Must be node_data_size bytes long.
  * @returns 0 on success, sets errno and returns -1 otherwise.
  */
-static inline int Queue_append(Queue_t* queue, const void* data) {
+int queue_append(queue_t* queue, const void* data);
+
+/**
+ * Clears a Queue, de-allocating every node and it's contents.
+ * @param queue The queue to be cleared.
+ * @returns 0 on success, sets errno and returns -1 otherwise.
+ */
+int queue_clear(queue_t* queue);
+
+/**
+ * Erases the first node without de-allocating the data.
+ * @param queue The queue that will be popped from.
+ * @returns Pointer to the data of the destroyed node, sets errno and returns NULL otherwise.
+ */
+void* queue_pop_head(queue_t* queue);
+
+#endif /* ANNYMOOSE_QUEUE_H */
+
+/*
+ *
+ */
+
+#ifdef ANNYMOOSE_QUEUE_IMPLEMENTATION
+#include <errno.h>
+#include <string.h>
+
+#define ANNYMOOSE_QUEUE_ERROR -1
+#define ANNYMOOSE_QUEUE_SUCCESS 0
+
+void queue_init(queue_t* queue, size_t data_size, void (*free_data)(void* data)) {
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->length = 0;
+    queue->node_data_size = data_size;
+    queue->free_data = free_data;
+}
+
+int queue_append(queue_t* queue, const void* data) {
     if (!queue) {
         errno = EINVAL;
         return ANNYMOOSE_QUEUE_ERROR;
     }
-    QueueNode_t* node = (QueueNode_t*)malloc(sizeof(QueueNode_t));
+    queue_node_t* node = (queue_node_t*)malloc(sizeof(queue_node_t));
     if (!node) {
         errno = ENOMEM;
         return ANNYMOOSE_QUEUE_ERROR;
@@ -72,12 +98,7 @@ static inline int Queue_append(Queue_t* queue, const void* data) {
     return ANNYMOOSE_QUEUE_SUCCESS;
 }
 
-/**
- * Clears a Queue, de-allocating every node and it's contents.
- * @param queue The queue to be cleared.
- * @returns 0 on success, sets errno and returns -1 otherwise.
- */
-static inline int Queue_clear(Queue_t* queue) {
+int queue_clear(queue_t* queue) {
     if (!queue) {
         errno = EINVAL;
         return ANNYMOOSE_QUEUE_ERROR;
@@ -85,7 +106,7 @@ static inline int Queue_clear(Queue_t* queue) {
 
     if (queue->length == 0) return ANNYMOOSE_QUEUE_SUCCESS; /* list already empty */
 
-    QueueNode_t *current_node = queue->head, *next_node = current_node;
+    queue_node_t *current_node = queue->head, *next_node = current_node;
     while (next_node) {
         current_node = next_node;
         next_node = current_node->next;
@@ -105,18 +126,13 @@ static inline int Queue_clear(Queue_t* queue) {
     return ANNYMOOSE_QUEUE_SUCCESS;
 }
 
-/**
- * Erases the first node without de-allocating the data.
- * @param queue The queue that will be popped from.
- * @returns Pointer to the data of the destroyed node, sets errno and returns NULL otherwise.
- */
-static inline void* Queue_pop_head(Queue_t* queue) {
+void* queue_pop_head(queue_t* queue) {
     if (!queue || !queue->head) {
         errno = EINVAL;
         return NULL;
     }
 
-    QueueNode_t* head = queue->head;
+    queue_node_t* head = queue->head;
     void* ret = head->data;
 
     queue->head = queue->head->next;
@@ -127,5 +143,4 @@ static inline void* Queue_pop_head(Queue_t* queue) {
     free(head);
     return ret;
 }
-
-#endif /* ANNYMOOSE_QUEUE_H */
+#endif /* implementation */
