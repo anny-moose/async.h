f1402edebe0ce186411d6f44e4af7bc7d9b90484
Annymoose <annymoosed@proton.me>
2026-03-12 16:25:23 +0100

    update examples/cancel_example.c

--- examples/cancel_example.c
+++ examples/cancel_example.c
@@ -1,4 +1,5 @@
 #include <err.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,12 +8,12 @@
 #include "async.h"
 
 void* other_func(void* params) {
-    sleep((long)params);
-    return params;
+    sleep((uintptr_t)params);
+    return NULL;
 }
 
 void* work_func(void* params) {
-    sleep((long)params);
+    sleep((uintptr_t)params);
     return params;
 }
 
@@ -44,7 +45,9 @@ int main(void) {
     promise_t* promises[PROMISES];
     int generated_promises = 0;
     for (int i = 0; i < PROMISES; i++) {
-        promises[generated_promises] = submit_task(&tq, (i % 3) ? work_func : other_func, (void*)(long)TIME);
+        promises[generated_promises] = submit_task(
+            &tq, (i % 3) ? work_func : other_func, (void*)(uintptr_t)TIME);
+        /* 1/3 submitted tasks are other_func */
         if (!promises[generated_promises]) {
             warn("promise %d not generated", i);
             continue;
@@ -61,26 +64,32 @@ int main(void) {
 
     for (int i = 0; i < generated_promises / 3; i++) {
         printf("Promise %d was ", i);
-        if (!await(promises[i])) /* awaiting a canceled promise returns NULL and sets errno to ECANCELED. Since our work function doesn't
-                                    return NULL, a simple null check is enough */
+        if (await(promises[i]) == ASYNC_INVALID_PTR)
+            /* awaiting a canceled promise returns
+              ASYNC_INVALID_PTR and sets errno to ECANCELED. Since no other
+              errors can occur when using normal await, it is sufficient to
+              check the return value */
             printf("canceled.\n");
         else
             printf("completed succesfully.\n");
     }
 
-    destroy_tq(&tq); /* destroying a thread queue cancels all pending promises */
+    /* destroying a thread queue cancels all pending promises */
+    destroy_tq(&tq);
+    printf("Thread queue destroyed.\n");
 
     for (int i = generated_promises / 3; i < generated_promises; i++) {
         printf("Promise %d was ", i);
-        if (!await(promises[i]))
+        if (await(promises[i]) == ASYNC_INVALID_PTR)
             printf("canceled.\n");
         else
             printf("completed succesfully.\n");
     }
 
     /*
-     * Notice how the first 4 regular promises always succeed, even though this type of task gets cancelled before they finish. That is
-     * because promises can not be cancelled mid-execution.
+     * Notice how the first 4 regular promises always succeed, even though this
+     * type of task gets cancelled before they finish. That is because promises
+     * can not be cancelled mid-execution.
      */
 
     return EXIT_SUCCESS;
